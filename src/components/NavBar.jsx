6c060cc1b442b8c2a85a723f82fc68bd36ea1ea8
daqirraty <Daqirraty@gmail.com>
2024-09-18 11:16:49 +0100

    pages folder added

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+
+
+function NavBar () {
+
+    return (
+         <div>
+            <ul className="navbar">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+         </div>
+    )
+}
+
+
+
+
+
+export default NavBar
